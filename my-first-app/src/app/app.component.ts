3c20b80b8db717ddadb21b2611a642f31b9f7829
souvikpal2000 <spal62588@gmail.com>
2022-03-19 14:19:55 +0530

    Password Generator Project

---
+++ my-first-app/src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-root',      
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+	title = 'Password Generator';
+
+	password  = '';
+	length = 0;
+	includeLength = false;
+	includeLetters = false;
+	includeNumbers = false;
+	includeSpecial = false;
+
+	setLength = (value: Event) => {
+		const parsedValue = parseInt((value.target as HTMLInputElement).value);
+		if(isNaN(parsedValue) == false){
+			this.length = parsedValue;
+			this.includeLength = true;
+		}
+	}
+	wantLetters = () => {
+		this.includeLetters = !this.includeLetters;
+	}
+	wantNumbers = () => {
+		this.includeNumbers = !this.includeNumbers;
+	}
+	wantSpecialChar = () => {
+		this.includeSpecial = !this.includeSpecial;
+	}
+
+	generatePassword = () => {
+		let validChars = '';
+		const letters = "abcdefghijklmnopqrstuvwxyz";
+		const numbers = "0123456789";
+		const special = "!@#$%^&*()<>{}[]/";
+		if(this.includeLetters){
+			validChars = validChars + letters;
+		}
+		if(this.includeNumbers){
+			validChars = validChars + numbers;
+		}
+		if(this.includeSpecial){
+			validChars = validChars + special;
+		}
+
+		let generatedPassword = '';
+		for(let i=0;i<this.length;i++){
+			const index = Math.floor(Math.random() * validChars.length);
+			generatedPassword = generatedPassword + validChars[index];
+		}
+		this.password = generatedPassword;
+	}
+}
